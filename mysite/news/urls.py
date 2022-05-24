8228aecb3a52d7781166396f8462908d7ae0adee
Lyova Parsyan <parsyan.1994@mail.ru>
2022-05-24 18:00:02 +0400

    saving html

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category'),
+    # """атрибут name позволяет задать для Джанго соответств. имя  c помощью тега url для маршрута, для динамичности
+    # если например название category поменять на cat, достаточно лишь в name указать новое имя"""
+
 ]
