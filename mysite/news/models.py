602b38d9d39e86b9e0c37fb6e9c6d0a8143c5e37
Lyova Parsyan <parsyan.1994@mail.ru>
2022-05-30 14:00:13 +0400

    saving changes

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория') #PROTECT-защита от удаления,
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория') #PROTECT-защита от удаления,
     # null=True чтобы с пустым полем можно было работать
     # "verbose_name позволяет, чтобы в админке данные поля назывались так, как удобно админу
     # 'Category' указывается строкой, так как она модель, которая создана после класса News, в противном случае ссылку
